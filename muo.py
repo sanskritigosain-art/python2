4989ae54621c10a41dd5cc974f80666c28b66215
sanskritigosain-art <sanskritigosain@gmail.com>
2025-08-27 13:17:10 +0530

    Add files via upload

---
+++ muo.py
@@ -0,0 +1,11 @@
+# Task 2: Sum of Integers from 1 to 50
+
+# Step 1: Initialize sum
+total = 0
+
+# Step 2: Use a for loop to add numbers from 1 to 50
+for i in range(1, 51):
+    total += i
+
+# Step 3: Display the result
+print(f"The sum of integers from 1 to 50 is: {total}")
